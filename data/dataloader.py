c1a488b6264cb7005ad06f4ee457bbd9eba9a795
65680 <wuuyuuhang@gmail.com>
2020-09-13 14:54:17 +0800

    first update

---
+++ data/dataloader.py
@@ -0,0 +1,74 @@
+import os
+from PIL import Image
+import numpy as np
+import os.path as osp
+import torch
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from torchvision.transforms import transforms
+
+transform_train_list = [
+        #transforms.RandomResizedCrop(size=128, scale=(0.75,1.0), ratio=(0.75,1.3333), interpolation=3), #Image.BICUBIC)
+        transforms.Resize((256,128), interpolation=3),
+        transforms.Pad(10),
+        transforms.RandomCrop((256,128)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]
+
+transform_test_list = [
+        transforms.Resize(size=(256,128),interpolation=3), #Image.BICUBIC
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]
+
+data_transform={'train':transform_train_list,
+                'test':transform_test_list}
+class ImageDataset(Dataset):
+    def __init__(self,dataset,transformer=None):
+        self.dataset=dataset
+        self.transformer=transformer
+    def __len__(self):
+        return len(self.dataset)
+    def __getitem__(self, index):
+        img_path,pid,cid=self.dataset[index]
+        img=read_image(img_path)
+        if self.transformer is not None:
+            img=self.transformer(img)
+        return img,pid,cid
+
+
+def read_image(img_path):
+    if not osp.exists(img_path):
+        raise IOError("{}DOES NOT EXIST".format(img_path))
+    try:
+        img=Image.open(img_path) .convert('RGB')
+    except IOError:
+        print("can't read {}".format(img_path))
+    return img
+
+if __name__=='__main__':
+    from mydataset_manager import Market1501
+    market=Market1501(root='D:\\engineer\ReIDDataset\Market')
+    train=market.train
+    transform_train = transforms.Compose(data_transform['train'])
+    train_loader=ImageDataset(train,transformer=transform_train)
+    # from torchvision.transforms import transforms
+    # transform=transforms.Compose([
+    #     transforms.CenterCrop(100),
+    #     transforms.RandomHorizontalFlip()
+    #
+    #
+    # ])
+
+    for batch,(img,pid,cid)in enumerate(train_loader):
+        print(img,pid,cid)
+        #Image._show(img_path)
+        plt.figure(12)
+        plt.subplot(121)
+        plt.imshow(img.permute([1,2,0]))
+        # img_t=transform(img)
+        # plt.subplot(122)
+        # plt.imshow(img_t.permute([1,2,0]))
+        plt.show()
---
+++ data/mydataset_manager.py
@@ -0,0 +1,63 @@
+import os
+import numpy as np
+import os.path as osp
+from IPython import embed
+from torch.utils.data import Dataset
+import glob
+import re
+class Market1501(object):
+    dataset_dir='Market'
+    def __init__(self,root):
+    #第一步，生成路径，检查路径是否存在
+        self.train_dir=osp.join(root,'bounding_box_train')
+        self.test_dir=osp.join(root,'bounding_box_test')
+        self.query_dir=osp.join(root,'query')
+        dir_to_check=[self.train_dir,self.test_dir,self.query_dir]
+        check_dir(dir_to_check)
+        self.train,self.train_num_pids,self.train_num_imgs=self._process_dir(self.train_dir,relabel=True)
+        self.test,self.test_num_pids,self.test_num_imgs =self._process_dir(self.test_dir, relabel=False)
+        self.query,self.query_num_pids,self.query_num_imgs=self._process_dir(self.query_dir,relabel=False)
+        pass
+    def _process_dir(self,dir,relabel=False):
+        #获取所有的图片路径，通过模式得到行人ID，再把ID变成1-751
+        img_paths=glob.glob(osp.join(dir,'*.jpg'))
+        pattern=re.compile(r'([\d]+)_c(\d)')
+        pid_container=set()
+        for img_path in img_paths:
+            pid,cid=map(int,pattern.search(img_path).groups())
+            if pid == -1: continue
+            pid_container.add(pid)
+        #把ID转换成LABEL
+        plabel={pid:label for label,pid in enumerate(pid_container)}
+        dataset=[]
+        for img_path in img_paths:
+            pid,cid=map(int,pattern.search(img_path).groups())
+            if pid == -1: continue
+            assert 0<=pid<=1501
+            assert 1<=cid<=6
+            cid-=1
+            #对于测试集不需要relabel
+            if relabel:
+                pid=plabel[pid]
+            dataset.append((img_path,pid,cid))
+        num_pids=len(pid_container)
+        num_imgs=len(img_paths)
+        return dataset,num_pids,num_imgs
+
+
+
+
+def check_dir(dir_to_check):
+    for i,path in enumerate(dir_to_check):
+        if not osp.exists(path):
+            raise RuntimeError('{} is not exist'.format(path))
+        print('{},no problem'.format(dir_to_check))
+def print_file_name(file_dir):
+    for root, dirs, files in os.walk(file_dir):
+        print(root) #当前目录路径
+        print(dirs) #当前路径下所有子目录
+        print(files) #当前路径下所有非目录子文件
+if __name__=='__main__':
+    market=Market1501('D:\\engineer\ReIDDataset\Market')
+    train_set,num_pids,num_imgs=market._process_dir(market.train_dir,relabel=True)
+    pass
